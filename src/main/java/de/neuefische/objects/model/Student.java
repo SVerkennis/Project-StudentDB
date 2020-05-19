28b781b26adecfe62472cbba5dd8c1cefb358c5a
Sabine Verkennis <s.verkennis@yahoo.de>
2020-05-19 20:27:21 +0200

    add new files

---
+++ src/main/java/de/neuefische/objects/model/Student.java
@@ -0,0 +1,68 @@
+package de.neuefische.objects.model;
+
+import java.util.Objects;
+
+public class Student {
+
+    //attribute bestimmen
+    private String flagColor;
+    private boolean single;
+    private int fails;
+
+
+    // zuweisen
+    public Student(String flagColor, boolean single, int fails) {
+        this.flagColor = flagColor;
+        this.single = single;
+        this.fails = fails;
+    }
+
+    // getter and setter generate - command + N
+    public String getFlagColor() {
+        return this.flagColor;
+    }
+
+    public void setFlagColor(String flagColor) {
+        this.flagColor = flagColor;
+    }
+
+    public boolean isSingle() {
+        return single;
+    }
+
+    public void setSingle(boolean single) {
+        this.single = single;
+    }
+
+    public int getFails() {
+        return fails;
+    }
+
+    public void setFails(int fails) {
+        this.fails = fails;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "flagColor='" + flagColor + '\'' +
+                ", single=" + single +
+                ", fails=" + fails +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return single == student.single &&
+                fails == student.fails &&
+                Objects.equals(flagColor, student.flagColor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(flagColor, single, fails);
+    }
+}
---
+++ src/test/java/de/neuefische/objects/model/StudentTest.java
@@ -0,0 +1,8 @@
+package de.neuefische.objects.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+
+}
