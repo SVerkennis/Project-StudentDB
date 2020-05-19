9799b667cb0122a9178df742c93e61a4aee402b6
Sabine Verkennis <s.verkennis@yahoo.de>
2020-05-19 20:59:11 +0200

    add testversion

--- src/test/java/de/neuefische/objects/model/StudentTest.java
+++ src/test/java/de/neuefische/objects/model/StudentTest.java
@@ -1,8 +1,53 @@
 package de.neuefische.objects.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentTest {
 
+
+    // Test for isSingle
+    @Test
+    public void ifStudentIsSingleThisShouldReturnTrue() {
+
+        //GIVEN
+        Student student1 = new Student("Blue", true, 5);
+
+        //WHEN
+        boolean result = student1.isSingle();
+
+        //THEN
+        assertEquals(true, result);
+    }
+
+
+    // Test for flagColor
+    @Test
+    public void ifStudentsFlagColorIsRedThisShouldReturnTrue() {
+
+        //GIVEN
+        Student student1 = new Student("Red", true, 5);
+
+        //WHEN
+        String result = student1.getFlagColor();
+
+        //THEN
+        assertEquals("Red", result);
+    }
+
+    // Test for getFails
+    @Test
+    public void ifStudentsHasFiveFailsThisShouldReturnTrue() {
+
+        //GIVEN
+        Student student1 = new Student("Red", true, 5);
+
+        //WHEN
+        int result = student1.getFails();
+
+        //THEN
+        assertEquals(5, result);
+    }
 
 }
